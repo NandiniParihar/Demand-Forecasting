d44830c32b470ad01560a70bf78253e5059e8772
Nandini Parihar <97394763+NandiniParihar@users.noreply.github.com>
2026-06-27 00:33:25 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -34,3 +34,58 @@ df.isnull().sum()
 df = df.dropna()
 
 
+categorical_cols = [
+    'Store ID',
+    'Product ID',
+    'Category',
+    'Region',
+    'Weather Condition',
+    'Seasonality'
+]
+
+for col in categorical_cols:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col].astype(str))
+
+
+X = df.drop('Demand', axis=1)
+y = df['Demand']
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+
+rf = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42,
+    n_jobs=-1
+)
+rf.fit(X_train, y_train)
+rf_pred = rf.predict(X_test)
+
+
+
+rf_mae = mean_absolute_error(y_test, rf_pred)
+
+rf_rmse = np.sqrt(
+    mean_squared_error(y_test, rf_pred)
+)
+
+rf_mape = np.mean(
+    np.abs((y_test-rf_pred)/y_test)
+)*100
+rf_r2 = r2_score(y_test, rf_pred)
+print("RF MAE:", rf_mae)
+print("RF RMSE:", rf_rmse)
+print("RF MAPE:", rf_mape)
+print("RF R2:", rf_r2)
+
+
+
+
+
